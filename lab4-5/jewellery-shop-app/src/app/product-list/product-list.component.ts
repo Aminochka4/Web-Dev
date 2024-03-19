1ce329e2a4eb1f263ce374f128d8741470d84de7
Aminochka <am_amanzholova@kbtu.kz>
2024-03-19 20:24:08 +0500

    recommit

---
+++ lab4-5/jewellery-shop-app/src/app/product-list/product-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products_by_category: Product[] | undefined;
+
+  // searchInput: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  share(id: number) {
+
+    const url = "http://localhost:4200/products/" + id;
+    const message = "Надо надо надо купить";
+    const telegramLink = `https://telegram.me/share/url?url=${url}&text=${message}`;
+
+    window.location.href = telegramLink;
+
+  }
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const categoryIdFromRoute = Number(routeParams.get('categoryId'));
+
+    this.products_by_category = products.filter(products => products.categoryId === categoryIdFromRoute);
+  }
+
+  likeProduct(product: Product): void {
+    if(product.likes==0){
+      product.likes += 1;
+    }else{
+      alert("You have already liked");
+    }
+    
+  }
+
+  deleteProduct(product: Product): void {
+    // @ts-ignore
+    const index = this.products_by_category.indexOf(product);
+    if (index > -1) {
+      // @ts-ignore
+      this.products_by_category.splice(index, 1);
+    }
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
